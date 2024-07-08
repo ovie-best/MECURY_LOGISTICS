e07ac856d595c8f528f0cacf4bb59100b516f8db
Ovie Nathaniel <ovienathaniel44@gmail.com>
2024-07-08 18:34:37 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          color: "green",
+        }}
+      >
+        Mecuri Logistics
+      </Text>
+      <Text>coming soon...</Text>
+    </View>
+  );
+}
